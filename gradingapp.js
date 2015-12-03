302050a31ac013d84110d805aa931fe7ed8e2c25
immortalkingkev <kevindamier@hotmail.com>
2015-12-03 22:11:52 +0000

    made the grading app work and change some styles

--- gradingapp.js
+++ gradingapp.js
@@ -6,5 +6,18 @@ function giveGrade() {
   alert("hey!");
 
 //Input your code below:
+  if (grade<100 && grade >90)
+        alert("A");
 
+  }
+  else if (grade<89 && grade >80) }
+      alert("B");
+    
+  }
+  else if (grade<79 && grade >70) {
+      alert("C");
+
+  }
+  else if (grade<69 && grade >60) }
+      alert("D");
   }
